49862466548cdbe24fcd79db4ce1e4aa9ad147ad
KingSuperZ <153145444+KingSuperZ@users.noreply.github.com>
2024-05-31 20:33:42 -0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -9,8 +9,8 @@ import numpy as np
 # Equation: x^3-5x    Derivative/Slope: 3x^2-5
 # Does the entire algorithm for gradient descent
 x = 3 # initial guess for the value of x
-lr = 0.01
-xlist = [x]
+lr = 0.01 # lr means learning rate which is the rate at which the points go down 
+xlist = [x] # list that starts with x and will contain the points that are created as it slowly goes to the local minimum
 for i in range(30):
     x = x-lr*(3*(x**2)-5)
     xlist.append(x)
